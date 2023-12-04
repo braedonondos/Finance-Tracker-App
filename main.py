a40550c27c0a52858759352b7b56f7a69296cf88
Ryan Esquino <ryanesquino@gmail.com>
2023-12-04 11:00:45 -0500

    DB dependencies added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, url_for, redirect, render_template, request, abort
+import psycopg2
 
 app = Flask(__name__)
 
@@ -7,6 +8,26 @@ users = {"username":"password"}
 
 @app.route("/")
 def default():
+    conn = psycopg2.connect("postgresql://postgres:sie_final_project@localhost:5432/postgres")
+    
+    # Creating a table example with some dummy data
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE users (username VARCHAR(255), password VARCHAR(255))")
+    cur.execute("INSERT INTO users (username, password) VALUES (\'user1234\', \'temp_pass\')")
+    
+    # Put all the data in the users table into the cursor
+    cur.execute("SELECT * FROM users")
+    result = cur.fetchall()
+
+    #Print all table data
+    for x in result:
+        print(x)
+    
+    #Delete all table data
+    cur.execute("DROP TABLE users")
+    
+
+    print("Connection to db successful...")
     print("Redirecting to login page...")
     return redirect(url_for("login"))
 
