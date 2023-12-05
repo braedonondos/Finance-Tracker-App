850915aad735e66fa996c8cc9193372cdb5f73f5
Ryan Esquino <ryanesquino@gmail.com>
2023-12-05 11:37:02 -0500

    Added database queries for expense data

--- main.py
+++ main.py
@@ -1,34 +1,20 @@
 from flask import Flask, url_for, redirect, render_template, request, abort
-# import psycopg2
+import psycopg2
 
 app = Flask(__name__)
 
 # Dummy user
-users = {"Bob":"1234"}
+users = {"Bob":"1234", "Alice":"4321"}
+conn = psycopg2.connect(
+    host="localhost",
+    database="finance_app",
+    user="postgres",
+    password="sie_final_project")
 
 @app.route("/")
 def default():
-    # conn = psycopg2.connect("postgresql://postgres:sie_final_project@localhost:5432/postgres")
-    
-    # # Creating a table example with some dummy data
-    # cur = conn.cursor()
-    # cur.execute("CREATE TABLE users (username VARCHAR(255), password VARCHAR(255))")
-    # cur.execute("INSERT INTO users (username, password) VALUES (\'user1234\', \'temp_pass\')")
-    
-    # # Put all the data in the users table into the cursor
-    # cur.execute("SELECT * FROM users")
-    # result = cur.fetchall()
-
-    # #Print all table data
-    # for x in result:
-    #     print(x)
-    
-    # #Delete all table data
-    # cur.execute("DROP TABLE users")
-    
-
-    # print("Connection to db successful...")
-    # print("Redirecting to login page...")
+    print("Connection to db successful...")
+    print("Redirecting to login page...")
     return redirect(url_for("login"))
 
 @app.route("/login/", methods=["GET", "POST"])
@@ -53,10 +39,18 @@ class Expense:
 
 @app.route("/profile/<username>")
 def homepage(username):
+    cur = conn.cursor()
+    
+    cur.execute("SELECT * FROM expenses_user WHERE username = \'" + username + "\'")
+    result = cur.fetchall()
 
-    expense1 = Expense("11/23/23", 500, "Groceries")
-    expense2 = Expense("11/25/23", 450, "Tech")
-    expenses = {expense1, expense2}
+    expense = None
+    expenses = []
+    
+    for x in result:
+        expense = Expense(x[3], x[1], x[0])
+        expenses.append(expense)
+    
     highlightExpense = None
     highPrice = 0
     for expense in expenses:
