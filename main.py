e090dbaba96625c970796ee8964dbf27d5df66c6
1GithubRon1 <122589050+1GithubRon1@users.noreply.github.com>
2023-12-04 19:44:08 -0500

    Update main.py
    Added Highlight Expense functionality

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 from flask import Flask, url_for, redirect, render_template, request, abort
-import psycopg2
+# import psycopg2
 
 app = Flask(__name__)
 
-# Temporary users database, database implementation needed
-users = {"username":"password"}
+# Dummy user
+users = {"Bob":"1234"}
 
 @app.route("/")
 def default():
-    conn = psycopg2.connect("postgresql://postgres:sie_final_project@localhost:5432/postgres")
+    # conn = psycopg2.connect("postgresql://postgres:sie_final_project@localhost:5432/postgres")
     
-    # Creating a table example with some dummy data
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE users (username VARCHAR(255), password VARCHAR(255))")
-    cur.execute("INSERT INTO users (username, password) VALUES (\'user1234\', \'temp_pass\')")
+    # # Creating a table example with some dummy data
+    # cur = conn.cursor()
+    # cur.execute("CREATE TABLE users (username VARCHAR(255), password VARCHAR(255))")
+    # cur.execute("INSERT INTO users (username, password) VALUES (\'user1234\', \'temp_pass\')")
     
-    # Put all the data in the users table into the cursor
-    cur.execute("SELECT * FROM users")
-    result = cur.fetchall()
+    # # Put all the data in the users table into the cursor
+    # cur.execute("SELECT * FROM users")
+    # result = cur.fetchall()
 
-    #Print all table data
-    for x in result:
-        print(x)
+    # #Print all table data
+    # for x in result:
+    #     print(x)
     
-    #Delete all table data
-    cur.execute("DROP TABLE users")
+    # #Delete all table data
+    # cur.execute("DROP TABLE users")
     
 
-    print("Connection to db successful...")
-    print("Redirecting to login page...")
+    # print("Connection to db successful...")
+    # print("Redirecting to login page...")
     return redirect(url_for("login"))
 
 @app.route("/login/", methods=["GET", "POST"])
@@ -45,9 +45,27 @@ def login():
         else:
             abort(404)
 
+class Expense:
+    def __init__(self, date, price, name):
+        self.date = date
+        self.price = price
+        self.name = name
+
 @app.route("/profile/<username>")
 def homepage(username):
-    return render_template("homepage.html", username = username)
+
+    expense1 = Expense("11/23/23", 500, "Groceries")
+    expense2 = Expense("11/25/23", 450, "Tech")
+    expenses = {expense1, expense2}
+    highlightExpense = None
+    highPrice = 0
+    for expense in expenses:
+        price = expense.price
+        if price > highPrice:
+            highPrice = price
+            highlightExpense = expense
+
+    return render_template("homepage.html", username = username, expenses = expenses, highlightExpense = highlightExpense)
 
 if __name__ == "__main__":
     app.run(debug=True)
